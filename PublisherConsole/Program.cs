e0e175a8d098991fbb895322e8caa71f9dc53f3a
PHIDELIST <phidelisoluoch@gmail.com>
2023-09-19 13:31:37 +0300

    initial commit

---
+++ PublisherConsole/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PublisherData;
+using PublisherDomain;
+
+using (PubContext context = new PubContext())
+{
+    context.Database.EnsureCreated();
+}
+GetAuthors();
+AddAuthor();
+GetAuthors();
+AddAuthorWithBook();
+GetAuthorsWithBooks();
+
+void AddAuthorWithBook()
+{
+    var author = new Author { FirstName = "Otonglo", LastName = "Ombulu" };
+    author.Books.Add(new Book { Title = "Programing Entity FrameWork", PublishDate = new DateTime(2009, 1, 1) });
+    author.Books.Add(new Book { Title = " C-sharp", PublishDate = new DateTime(2010, 8, 1) });
+    using var context = new PubContext();
+    context.Authors.Add(author);
+    context.SaveChanges();
+}
+
+void GetAuthorsWithBooks()
+{
+    using var context = new PubContext();
+    var authors = context.Authors.Include(a=> a.Books).ToList();
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+        foreach (var book in author.Books)
+        {
+            Console.WriteLine("*"+book.Title);
+        }
+    }
+
+}
+void AddAuthor()
+{
+    var author = new Author { FirstName = "Phidelist", LastName = "Oluoch" };
+    using var context = new PubContext();
+    context.Authors.Add(author);
+    context.SaveChanges();
+}
+void GetAuthors()
+{
+    using var context = new PubContext();
+    var authors = context.Authors.ToList();
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName );
+    }
+}
