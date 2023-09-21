eebc92b0e9a0087c0544323685ea341dee772183
PHIDELIST <phidelisoluoch@gmail.com>
2023-09-21 17:02:47 +0300

    migrations wip

--- PublisherData/PubContext.cs
+++ PublisherData/PubContext.cs
@@ -14,5 +14,23 @@ namespace PublisherData
                 "Data Source = (localdb)\\MSSQLLOCALDB; Initial Catalog = PubDatabase"
                 );
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>().HasData(
+                new Author { Id = 1, FirstName = "Rhoda", LastName = "Okeyo" });
+            var authorList = new Author[]
+            {
+                new Author { Id = 2, FirstName = "Rhoda", LastName = "Okeyo" },
+                new Author { Id = 3, FirstName = "Rhoda", LastName = "Okeyo" },
+                new Author { Id = 4, FirstName = "Rhoda", LastName = "Okeyo" },
+                new Author { Id = 5, FirstName = "Rhoda", LastName = "Okeyo" },
+                new Author { Id = 6, FirstName = "Rhoda", LastName = "Okeyo" },
+                new Author { Id = 7, FirstName = "Rhoda", LastName = "Okeyo" },
+                new Author { Id = 8, FirstName = "Rhoda", LastName = "Okeyo" }
+            };
+            modelBuilder.Entity<Author>().HasData(authorList );
+
+        }
     }
 }
